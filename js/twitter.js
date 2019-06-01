dbc08a69479c735ed268e9f86b0e797c3109ccc6
Babacar Cissé Dia <contact@babacar-cisse-dia.com>
2019-06-01 23:35:57 +0000

    added readme

--- js/twitter.js
+++ js/twitter.js
@@ -7,7 +7,7 @@ var driver = neo4j.driver('bolt://34.203.33.130:36490', neo4j.auth.basic('neo4j'
 var query = "MATCH \
   (follower:User)-[:FOLLOWS]->(u:User:Me) \
   RETURN  \
-  follower.screen_name AS user, follower.followers AS followers \
+  follower.screen_name AS user, follower.followers AS followers, follower \
   ORDER BY \
   followers DESC";
 
@@ -17,7 +17,7 @@ let session = driver.session();
 
 session.run(query, params)
   .then(function(result) {
-    result.records.forEach(function(record) {
+    result.records.forEach(function(record)
       console.log(record.get('user') + ": " + record.get('followers').toInt());
     })
   })
